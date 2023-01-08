e92c93ac4e926dde60e8754df824f23edb4eadfa
Luis Borit <luisalbertoboritguitton@gmail.com>
2023-01-08 16:05:51 -0500

    added lint, and handle submit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,46 +1,39 @@
 import styles from "./Login.module.css"
 import { useState } from "react"
 import axios from "axios"
-const initialFormState = {
-    username: "",
-    password: ""
-}
 
 export default function Login () {
-    const [formState, setFormState] = useState(initialFormState)
-    const handleSubmit = (e) => {
+    const headers = {
+          'Content-Type': 'application/json',
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        
         const sendToAuth = async () => {
-            await axios.post('http://127.0.0.1:8000/auth/login', {
-                    firstName: 'Fred',
-                    lastName: 'Flintstone'
-                })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
+            return await axios.post('http://127.0.0.1:8000/auth/login', {
+                "username": e.target.username.value,
+                "password": e.target.password.value
+            }).catch(function (error) {
                     console.log(error);
                 })
         }
         sendToAuth()
     }
 
-        
-
-    const handleKeyUp = (e) => {
-        e.preventDefault()
-        setFormState({...formState, [e.target.name]:e.target.value})
-    }
-
     return (
         <div className={styles.formContainer}>
-            <form className="loginForm" onSubmit={handleSubmit} onKeyUp={handleKeyUp}>
-                <label className="form-label" htmlFor="username">Usuario</label>
-                <input type="text" name="username" id="username" className="form-control"/>
-                <br />
-                <label htmlFor="password" className="form-label">Contraseña</label>
-                <input type="password" name="password" id="password" className="form-control"/>
-                <br />
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <div>
+                    <label className="form-label" htmlFor="username">Usuario</label>
+                    <input type="text" name="username" id="username" className="form-control"/>
+                    <br />
+                </div>
+                <div>    
+                    <label htmlFor="password" className="form-label">Contraseña</label>
+                    <input type="password" name="password" id="password" className="form-control"/>
+                    <br />
+                </div>
                 <button className="form-control btn-info">Iniciar Sesion</button>
             </form>
         </div>
